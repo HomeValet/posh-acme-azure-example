f2478ee48bfcdf2f78ded64b536200930901881f
Toby Meyer <toby.meyer@homevalet.co>
2020-11-19 21:49:12 -0600

    Hosted agents azcopy command defaults to version 7, but we are using 10, so we need to test for the azcopy10 alias and use it if it is there

--- New-AcmeCertificate.ps1
+++ New-AcmeCertificate.ps1
@@ -18,7 +18,14 @@ Remove-Item $workingDirectory -Recurse -ErrorAction Ignore
 New-Item -Path $workingDirectory -ItemType Directory | Out-Null
 
 # Sync contents of storage container to working directory
-azcopy sync "$StorageContainerSASToken" "$workingDirectory"
+# For the script to handle both a MSFT hosted agent that defaults azcopy version 10 to azcopy and our maintained agent that defaults version 10 to azcopy, we need a non-erroring test
+if (get-alias -Name azcopy10 -ErrorAction SilentlyContinue){
+    azcopy10 sync "$StorageContainerSASToken" "$workingDirectory"
+}
+else {
+    azcopy sync "$StorageContainerSASToken" "$workingDirectory"
+}
+
 
 # Set Posh-ACME working directory
 $env:POSHACME_HOME = $workingDirectory
@@ -42,4 +49,10 @@ $pArgs = @{ CFTokenInsecure = $CloudFlareAPIToken }
 New-PACertificate -Domain $CertificateNamesArr -DnsPlugin Cloudflare -PluginArgs $pArgs
 
 # Sync working directory back to storage container
-azcopy sync "$workingDirectory" "$StorageContainerSASToken"
+# For the script to handle both a MSFT hosted agent that defaults azcopy version 10 to azcopy and our maintained agent that defaults version 10 to azcopy, we need a non-erroring test
+if (get-alias -Name azcopy10 -ErrorAction SilentlyContinue){
+    azcopy10 sync "$workingDirectory" "$StorageContainerSASToken"
+}
+else {
+    azcopy sync "$workingDirectory" "$StorageContainerSASToken"
+}
